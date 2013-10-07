be44c0c4234b56b3752b2985aadc4a83744bc6a7
srini <srinivasiyerb@gmail.com>
2013-10-07 13:03:54 +0530

    hil

--- Gitub/src/com/relevanceLab/GitTest/GitTest.java
+++ Gitub/src/com/relevanceLab/GitTest/GitTest.java
@@ -4,8 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		Sytem.out.println("Git is fun");
-		Sytem.out.println("Git is fun");
-		Sytem.out.println("Git is fun");
+		
 	}
 
 }
